aa5a47aa2a2f57d8d3d9b4c306cec80b5e6c3a1b
wangyujie <2535310040@qq.com>
2021-01-22 16:08:17 +0800

    第一周作业 任务二

---
+++ week1/src/Task2.java
@@ -0,0 +1,12 @@
+public class Task2 {
+    public static void main(String[] args) {
+        int i=23;
+        int j=55;
+        System.out.println("i="+i+",j="+j);
+        i=i+j;
+        j=i-j;
+        i=i-j;
+        System.out.println("i="+i+",j="+j);
+    }
+
+}
