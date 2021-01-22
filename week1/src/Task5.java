09e5c625378c1471d441e794cdd5a1b12294060c
wangyujie <2535310040@qq.com>
2021-01-22 17:38:08 +0800

    第一周作业 任务五

--- week1/src/Task5.java
+++ week1/src/Task5.java
@@ -1,12 +1,36 @@
+import java.util.Scanner;
+
+class Circle{
+    private double radius;
+    public Circle(){}
+    public Circle(double radius)
+    {
+        this.radius=radius;
+    }
+    public double getArea(){
+        return Math.PI*Math.pow(this.radius,2);
+    }
+    public double getPerimeter()
+    {
+        return 2*Math.PI*this.radius;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+}
 public class Task5 {
     public static void main(String[] args) {
-        int i=23;
-        int j=55;
-        System.out.println("i="+i+",j="+j);
-        i=i+j;
-        j=i-j;
-        i=i-j;
-        System.out.println("i="+i+",j="+j);
+        Scanner in=new Scanner(System.in);
+        System.out.println("请输入圆的半径：");
+        double radius=in.nextDouble();
+        Circle c=new Circle(radius);
+        System.out.printf("the area is %.2f\n",c.getArea());
+        System.out.printf("the area is %.2f\n",c.getPerimeter());
+
     }
 
 }
