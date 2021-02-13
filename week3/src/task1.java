27543c2a9e7dc6a4234b362e19543257b77db039
wangyujie <2535310040@qq.com>
2021-02-13 17:31:39 +0800

    第三周作业 任务一

---
+++ week3/src/task1.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class task1 {
+    public static void main(String[] args) {
+        int nums[]={1,2,3,-1,-2,0};
+        List<List<Integer>> a=threeSum(nums,0);
+        for (List<Integer> k:a)
+        {
+            System.out.println(k);
+        }
+    }
+    public  static List<List<Integer>> threeSum(int[] nums,int target) {
+        List<List<Integer>> a=new ArrayList<>();
+        for(int i=0;i<nums.length-2;i++)
+        {
+            for(int j=i+1;j<nums.length-1;j++)
+            {
+                for(int k=j+1;k<nums.length;k++)
+                {
+                    if(nums[i]+nums[j]+nums[k]==target)
+                    {
+                        List<Integer> m=new ArrayList<>();
+                        m.add(nums[i]);
+                        m.add(nums[j]);
+                        m.add(nums[k]);
+                        a.add(m);
+                    }
+                }
+            }
+        }
+        return a;
+    }
+}
