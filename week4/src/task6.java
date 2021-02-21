b6a590a7b0d8e231ffd40504f13371bc09dc2310
wangyujie <2535310040@qq.com>
2021-02-21 18:20:33 +0800

    第四周作业 任务六

---
+++ week4/src/task6.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class task6 {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        Stack<String> stack=new Stack<>();
+        System.out.println("输入数组长度：");
+        int n=in.nextInt();
+        String[] str=new String[n];
+        for(int i=0;i<n;i++)
+        {
+            str[i]=in.next();
+
+        }
+        System.out.println(findStack(str));
+    }
+    public static boolean findStack(String[] str)
+    {
+        Stack<String> stack=new Stack<>();
+        for(int i=0;i<str.length;i++)
+        {
+            if(stack.isEmpty())
+            {
+                stack.push(str[i]);
+            }
+            else{
+                String p = stack.peek();
+                if ((p.equals("(") && str[i].equals(")")) || (p.equals("[") && str[i].equals("]")) || (p.equals("{") && str[i].equals("}"))) {
+                    stack.pop();//遇到“）” “】” “}”则出栈
+                } else {
+                    stack.push(str[i]);
+                }
+            }
+        }
+        if(stack.isEmpty())
+        {
+            return  true;
+        }else
+        {
+            return false;
+        }
+
+    }
+}
