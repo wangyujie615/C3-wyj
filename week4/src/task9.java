78257761c88129efa3993724124db3530710d9d7
wangyujie <2535310040@qq.com>
2021-02-22 16:53:41 +0800

    第四周作业 任务九

---
+++ week4/src/task9.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class task9 {
+    public static void main(String[] args) throws Exception{
+        File inputFile=new File("c:"+File.separator+"csatest"+File.separator+"demo.txt");
+        File outFile=new File("c:"+File.separator+"csatest2"+File.separator+"demo2.txt");
+        if(!outFile.getParentFile().exists())
+        {
+            outFile.getParentFile().mkdirs();
+        }
+        OutputStream in=new FileOutputStream(outFile);
+        InputStream out=new FileInputStream(inputFile);
+        byte[] data=new byte[1024];
+        int temp=0;
+        while((temp=out.read(data))!=-1)
+        {
+            in.write(data,0,temp);
+        }
+        out.close();
+        in.close();
+    }
+}
