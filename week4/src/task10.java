4a677fe07439c567df73ab0f4668274cd907d11e
wangyujie <2535310040@qq.com>
2021-02-23 14:53:31 +0800

    第四周作业 任务十

--- week4/src/task10.java
+++ week4/src/task10.java
@@ -1,5 +1,32 @@
-public class task10 {
+class MyThread implements Runnable{
+    private int count=0;
+    private static final Object obj=new Object();
+    @Override
+    public void run() {
+        while (count<=10)
+        {
+            synchronized (obj)
+            {
+                System.out.println(Thread.currentThread().getName()+":"+this.count++);
+                obj.notify();//唤醒下一个进程
+                if(count<=10)
+                {
+                   try {
+                       obj.wait();//该进程等待
+                   }catch (Exception e)
+                   {
+                       e.printStackTrace();
+                   }
+                }
+            }
+        }
+    }
+}
+ class task10 {
     public static void main(String[] args) {
+        MyThread myThread=new MyThread();
+        new Thread(myThread,"线程A").start();
+        new Thread(myThread,"线程B").start();
 
     }
 }
